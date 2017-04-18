a97aa35c83ac459ecd3372250ee6a03705432c1c
pensiero <oscar.nesis@gmail.com>
2017-04-18 15:08:25 +0200

    Table colorized fixes

--- src/AppBundle/Resources/js/main.js
+++ src/AppBundle/Resources/js/main.js
@@ -3,11 +3,21 @@ $(function() {
     // floating table head
     $('#content > table').floatThead();
 
+
+    // graphup cleaner
+    $.fn.graphup.cleaners.cleanTableCell = function(value, options) {
+        return value.replace('.', '').replace('€', '').trim();
+    };
+
     // graphup
     $('#content > table td.graph1').graphup({
+        cleaner: 'cleanTableCell',
+        decimalPoint: ',',
         colorMap: 'burn'
     });
     $('#content > table td.graph2').graphup({
+        cleaner: 'cleanTableCell',
+        decimalPoint: ',',
         colorMap: 'greenPower'
     });
 
