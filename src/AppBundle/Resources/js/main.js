44bdc52e9ce40cb605171436722345d36b929499
pensiero <oscar.nesis@gmail.com>
2017-04-21 20:54:02 +0200

    Fixed graphup method call

--- src/AppBundle/Resources/js/main.js
+++ src/AppBundle/Resources/js/main.js
@@ -5,18 +5,17 @@ $(function() {
 
 
     // graphup cleaner
-    $.fn.graphup.cleaners.cleanTableCell = function(value, options) {
+    $.fn.graphup.cleaners.cleanCellWithPrice = function(value, options) {
         return value.replace('.', '').replace('€', '').trim();
     };
 
     // graphup
     $('#content > table td.graph1').graphup({
-        cleaner: 'cleanTableCell',
+        cleaner: 'cleanCellWithPrice',
         decimalPoint: ',',
         colorMap: 'burn'
     });
     $('#content > table td.graph2').graphup({
-        cleaner: 'cleanTableCell',
         decimalPoint: ',',
         colorMap: 'greenPower'
     });
